5c0f2cef47773e8be7bfa28dfbd9aec4886a67db
Seberyak <v_liparteliani@cu.edu.ge>
2018-10-27 01:03:12 +0400

    added first project file

---
+++ index_atom.js
@@ -0,0 +1,70 @@
+var a, b, c, d;
+var answr="answr";
+var a = "hello";
+a="";
+a.substring(0, a.length - 1);
+function plus() {};
+
+function minus() {};
+
+function mult() /*გამრავლება*/ {};
+
+function dev() /*გაყოფა*/ {
+
+}
+
+/*function equal() {
+  for(var i=0;i<a.length;i++){
+      if(0<=a[i]<=9) continue;
+        else if(a[i]=='.'){}
+        else if (a[i]=='±') {}
+        else if (a[i]=='!') {}
+      switch(a[i]){
+        case ''
+
+
+
+
+      }
+
+
+
+  }
+  document.getElementById("ans").innerHTML = answr;
+  a="";
+}*/
+function c1() /*clear*/ {
+   if(a.length > 1){
+      a = a.substring(0, a.length - 1);
+      document.getElementById("ans").innerHTML = a;
+  }
+  else{ document.getElementById("ans").innerHTML = "0";}
+  console.log(a);
+
+}
+
+function ce() {
+  a = "";
+  document.getElementById("ans").innerHTML = "0";
+}
+
+function fact() {
+
+}
+
+function squrt() {
+
+}
+function equal() {
+  document.getElementById("ans").innerHTML=answr;
+}
+
+function butt(val) {
+
+
+
+
+  a+=val;
+  document.getElementById("ans").innerHTML = a;
+  console.log(a);
+}

4e01c1fa1b5908a9710012a049dcbf0c78c28246
Seberyak <v_liparteliani@cu.edu.ge>
2018-12-14 03:34:01 +0400

    Update index_atom.js

--- index_atom.js
+++ index_atom.js
@@ -1,39 +1,20 @@
+
 var request, c, d;
-var answr="answr";
-var request = "hello";
+var answr=new String("answr");
+
 request="0";
+var str= "1234567890()";
 request.substring(0, request.length - 1);
-function plus() {};
 
-function minus() {};
-
-function mult() /*გამრავლება*/ {};
-
-function dev() /*გაყოფა*/ {
-
+var audio = new Audio();
+function playAudio() {
+  audio.src = "sound.ogg";
+  audio.autoplay = true;
 }
 
-/*function equal() {
-  for(var i=0;i<request.length;i++){
-      if(0<=request[i]<=9) continue;
-        else if(request[i]=='.'){}
-        else if (request[i]=='±') {}
-        else if (request[i]=='!') {}
-      switch(request[i]){
-        case ''
 
-
-
-
-      }
-
-
-
-  }
-  document.getElementById("ans").innerHTML = answr;
-  request="";
-}*/
-function c1() /*clear*/ {
+function c1() /*clear last element*/ {
+  playAudio();
    if(request.length > 1){
       request = request.substring(0, request.length - 1);
       document.getElementById("ans").innerHTML = request;
@@ -47,6 +28,7 @@ function c1() /*clear*/ {
 }
 
 function ce() /*clear all*/ {
+  playAudio();
   request = "0";
   document.getElementById("ans").innerHTML = request;
   console.log(request);
@@ -54,10 +36,17 @@ function ce() /*clear all*/ {
 }
 
 function fact() {
-
+  playAudio();
+  let a=eval(request);
+  let buff=1;
+  for(var i=a;i>0;i--)buff*=i;
+  request=buff.toString();
+  console.log(buff);
+  document.getElementById('ans').innerHTML=request;
 }
 
 function dot() {
+  playAudio();
   if(request.includes('.')==false ){
     request+=".";
     document.getElementById("ans").innerHTML = request;
@@ -66,19 +55,50 @@ function dot() {
 }
 
 function squrt() {
-if(request=="0") request="√";
-document.getElementById("ans").innerHTML = request;
+playAudio();
+let a;
+a=eval(request);
+a=Math.sqrt(a);
+console.log(a);
+request=a.toString();
+document.getElementById("ans").innerHTML=request;
+}//vwert fesvs dasawyisshi an romelime operaciis shemdeg
 
+function sqr(){
+  playAudio();
+  let a=eval(request);
+  console.log(Math.pow(a,2));
+  a=Math.pow(a,2);
+  request=a.toString();
+  document.getElementById("ans").innerHTML=request;
 }
 
 function equal() {
-  document.getElementById("ans").innerHTML=answr;
+  playAudio();
+  request=eval(request).toString();
+  console.log(request);
+  document.getElementById("ans").innerHTML=request;
+
 }
 
+function opposite() {
+  playAudio();
+  if(request[0]!="-"){
+  var st="-";
+  request=st.concat(request);
+  document.getElementById("ans").innerHTML = request;
+  console.log(request );}//tu ricxvi dadebitia
+  else{ request=request.slice(1);
+        document.getElementById("ans").innerHTML = request;
+          console.log(request ); }}
+// tu ricxvi uaryofitia
+//es metodi gamoviyenot mxolod pirveli ricxvistvis
+
 function butt(val) {
+  playAudio();
   var bool=false;
-    for(var i=0;i<10;i++){
-        if(val==i){bool=true;break;
+    for(var i=0;i<str.length;i++){
+        if(val==str[i]){bool=true;break;
         }
 }
   if(request=="0" && bool==true) {
